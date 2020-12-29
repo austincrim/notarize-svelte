7daf3fb5c342e10629a184056b0623ceabddbe06
Austin Crim <crim.austin@principal.com>
2020-12-28 22:12:49 -0600

    use literal url

--- src/stores/notesStore.js
+++ src/stores/notesStore.js
@@ -1,5 +1,7 @@
 import { writable } from 'svelte/store';
 
-const notes = fetch(`${process.env.URL}/.netlify/functions/hello`).then(res => res.json());
+const notes = fetch(
+    `https://vibrant-snyder-1d0131.netlify.app/.netlify/functions/hello`
+).then(res => res.json());
 
 export const notesStore = writable(notes);
