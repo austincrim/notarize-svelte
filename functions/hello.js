b5fbb230e5ae81b9313a48e1aa6d76848c8f5040
Austin Crim <crim.austin@principal.com>
2020-12-28 21:47:46 -0600

    test env var

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'hello world' }),
+        body: JSON.stringify({ message: `Environment ${process.env.HELLO}` }),
     };
 };
